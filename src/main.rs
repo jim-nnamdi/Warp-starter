140b471ef75f277ec4b34bea4670291590ae5afe
jim-nnamdi <jimsamuel50@gmail.com>
2022-02-02 08:38:16 +0100

    + start entry files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+const TEMPLATEDIRECTORY : &str = "templates/";
+
+#[tokio::main]
+async fn main() {
+
+    let content = warp::fs::dir(TEMPLATEDIRECTORY);
+
+    let root = warp::get().and(warp::path::end()).and(warp::fs::file(format!("{}/index.html", TEMPLATEDIRECTORY)));
+
+    let webroutes = content.or(root);
+
+    warp::serve(webroutes).run(([127,0,0,1], 7900)).await;
 }
